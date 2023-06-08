2c263246a196ff073e99071ab868292779f28fd7
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2023-06-08 08:55:59 -0600

    subir archivo llamando libreriras

---
+++ code/01_librerias.R
@@ -0,0 +1,33 @@
+## RNA-seq
+```{r librerias_rna-seq, message=FALSE, warning=FALSE}
+suppressPackageStartupMessages(library("edgeR"))
+suppressPackageStartupMessages(library("ExploreModelMatrix"))
+suppressPackageStartupMessages(library("limma"))
+suppressPackageStartupMessages(library("recount3"))
+suppressPackageStartupMessages(library("clusterProfiler"))
+suppressPackageStartupMessages(library("org.Mm.eg.db"))
+suppressPackageStartupMessages(library(AnnotationDbi))
+```
+
+## Rendimiento
+```{r librerias_rendimiento, message=FALSE, warning=FALSE}
+suppressPackageStartupMessages(library('stringr'))
+suppressPackageStartupMessages(library("biocthis"))
+suppressPackageStartupMessages(library("here"))
+suppressPackageStartupMessages(library("postcards"))
+suppressPackageStartupMessages(library("pryr"))
+suppressPackageStartupMessages(library("usethis"))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library('plyr'))
+```
+## Tablas
+```{r librerias_tablas, message=FALSE, warning=FALSE}
+suppressPackageStartupMessages(library("SummarizedExperiment"))
+suppressPackageStartupMessages(library("iSEE"))
+suppressPackageStartupMessages(library("ggplot2"))
+suppressPackageStartupMessages(library("patchwork"))
+suppressPackageStartupMessages(library("pheatmap"))
+suppressPackageStartupMessages(library("RColorBrewer"))
+suppressPackageStartupMessages(library(ggpubr))
+```
